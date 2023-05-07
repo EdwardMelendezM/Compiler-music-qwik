b660607c8c6a3db0de78804c37335d804f57f7f6
EdwardMelendezM <192666@unsaac.edu.pe>
2023-05-07 14:52:22 -0500

    adding tailwind and remove unnecessary files and paths

--- src/root.tsx
+++ src/root.tsx
@@ -1,6 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 import { QwikCityProvider, RouterOutlet, ServiceWorkerRegister } from '@builder.io/qwik-city';
-import { RouterHead } from './components/router-head/router-head';
 
 import './global.css';
 
@@ -15,7 +14,6 @@ export default component$(() => {
     <QwikCityProvider>
       <head>
         <meta charSet="utf-8" />
-        <RouterHead />
       </head>
       <body lang="en">
         <RouterOutlet />

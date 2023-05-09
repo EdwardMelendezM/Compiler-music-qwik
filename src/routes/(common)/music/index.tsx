35d624fffe17b5494aba9c16d68407637aac2f8d
EdwardMelendezM <192666@unsaac.edu.pe>
2023-05-09 11:49:35 -0500

    adding Form of qwik and routeAction$

--- src/routes/(common)/music/index.tsx
+++ src/routes/(common)/music/index.tsx
@@ -1,9 +1,36 @@
 import { component$ } from "@builder.io/qwik";
+import { routeAction$,Form } from "@builder.io/qwik-city";
+
+export const useLoginAction = routeAction$((data,{cookie,redirect})=>{
+  console.log("Aqui se envio toda la data");
+  function validate(data:any){
+    if (data.username.lenth < 4 || data.password.lenth < 5){
+      return false
+    }
+    else{
+      return true
+    }
+  }
+  function getAuthToken(data:any){
+    return 'llave '+data
+  }
+  
+  if(validate(data)){
+    console.log("Esta validado");
+    cookie.set('auth',getAuthToken(data.username))
+    throw redirect(302,'/')
+   }
+})
 
 export default component$(()=>{
+  const login = useLoginAction()
   return(
-    <div>
-      Music
+    <div >
+      <Form action={login}>
+        <input type="text" name="username" />
+        <input type="password" name="password" />
+        <button type="submit" >Login</button>
+      </Form>
     </div>
   )
 })
